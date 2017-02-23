8a6479f115d7b7a9ddd76456089d86d8535b256c
Mathias <tympanix@gmail.com>
2017-02-23 17:35:23 +0100

    Use buffered writer when generating data

--- example/dup.go
+++ example/dup.go
@@ -1,6 +1,7 @@
 package example
 
 import (
+	"bufio"
 	"io"
 	"log"
 )
@@ -14,9 +15,12 @@ type DupEvent struct {
 // Trigger start the dubpication producing A characters
 func (d DupEvent) Trigger(w *io.PipeWriter) {
 	defer w.Close()
+	bw := bufio.NewWriter(w)
+
 	for i := 0; i < d.Length; i++ {
-		if _, err := w.Write([]byte(d.String)); err != nil {
+		if _, err := bw.Write([]byte(d.String)); err != nil {
 			log.Fatal(err)
 		}
 	}
+	bw.Flush()
 }
