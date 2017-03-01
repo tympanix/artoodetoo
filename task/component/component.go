057ec6029f7c5b4d1a2783d41dcc0272affdea93
Mathias <tympanix@gmail.com>
2017-03-01 21:26:03 +0100

    Ingredient struct for defining inputs for components

--- task/component/component.go
+++ task/component/component.go
@@ -3,13 +3,32 @@ package component
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"reflect"
 
 	"github.com/Tympanix/automato/task"
+	"github.com/Tympanix/automato/task/action"
+	"github.com/Tympanix/automato/task/converter"
+	"github.com/Tympanix/automato/task/event"
+)
+
+const (
+	id     = "id"
+	output = "output"
+	input  = "input"
 )
 
 // New creates a new component from events, actions and converters
 func New(c interface{}) *Component {
+	switch t := c.(type) {
+	case event.Event:
+	case converter.Converter:
+	case action.Action:
+		break
+	default:
+		log.Fatalf("Cannot create component from type %T", t)
+	}
+
 	return &Component{
 		id:        componentName(c),
 		component: c,
@@ -28,8 +47,10 @@ func componentName(component interface{}) string {
 
 // Component wraps the elements of the application and extends it's functionality.
 type Component struct {
-	id        string
-	component interface{}
+	id          string
+	name        string
+	ingredients []task.Ingredient
+	component   interface{}
 }
 
 // ID returns the id of the component
@@ -53,20 +74,38 @@ func (c *Component) Input() interface{} {
 	return nil
 }
 
+// IsEvent returns whether the component is an event or not
+func (c *Component) IsEvent() bool {
+	_, ok := c.component.(event.Event)
+	return ok
+}
+
+// IsAction returns whether the component is an action or not
+func (c *Component) IsAction() bool {
+	_, ok := c.component.(action.Action)
+	return ok
+}
+
+// IsConverter returns whether the component is a converter or not
+func (c *Component) IsConverter() bool {
+	_, ok := c.component.(converter.Converter)
+	return ok
+}
+
 // MarshalJSON returns a json representation of the component. The json representation
 // can be used by frontsends to inspect the components type, it's identification and
 // the input/output is can handle.
 func (c *Component) MarshalJSON() ([]byte, error) {
 	fmt.Println("Calling marshal")
 	m := make(map[string]interface{})
-	m["id"] = c.ID()
+	m[id] = c.ID()
 
 	if out := c.Output(); out != nil {
-		m["output"] = describeIO(out)
+		m[output] = describeIO(out)
 	}
 
 	if in := c.Input(); in != nil {
-		m["input"] = describeIO(in)
+		m[input] = describeIO(in)
 	}
 
 	return json.Marshal(&m)
--- task/task.go
+++ task/task.go
@@ -3,16 +3,27 @@ package task
 import (
 	"fmt"
 
-	"github.com/Tympanix/automato/task/action"
-	"github.com/Tympanix/automato/task/converter"
-	"github.com/Tympanix/automato/task/event"
+	"github.com/Tympanix/automato/task/component"
+)
+
+const (
+	variable = iota
+	static   = iota
+	finish   = iota
 )
 
 // Task is an object that processes data based on events, converters and actions
 type Task struct {
-	Event      event.Event
-	Converters []converter.Converter
-	Action     action.Action
+	Event   component.Component
+	Actions []component.Component
+}
+
+// Ingredient describes a variable or static value. If the source is a variable
+// it will be a string representation of which component the ingredient links to.
+// The frontend will use ingredients to define input for components is json format
+type Ingredient struct {
+	Type  int
+	Value interface{}
 }
 
 // Run starts the given task by connecting all components by pipes
