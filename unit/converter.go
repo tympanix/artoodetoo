ace1be1a2dd0516275d7b17f9bd060427c8085e0
Mathias <tympanix@gmail.com>
2017-03-08 15:47:16 +0100

    Deleted old converter & event interface

--- unit/converter.go
+++
@@ -1,8 +0,0 @@
-package unit
-
-// Converter interface describes an object which can convert one data stream to another
-type Converter interface {
-	Convert()
-	Input() interface{}
-	Output() interface{}
-}
--- unit/event.go
+++
@@ -1,7 +0,0 @@
-package unit
-
-// Event interface describes an event
-type Event interface {
-	Trigger() error
-	Output() interface{}
-}
