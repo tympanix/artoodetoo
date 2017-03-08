26c2ae497fd06b045c802c5d2f63a0a070972ea4
Mathias <tympanix@gmail.com>
2017-03-08 21:42:38 +0100

    Renamed to common.go / Updated unit map parsing

--- util/unitutil.go
+++ util/unitutil.go
@@ -2,10 +2,13 @@ package util
 
 import "github.com/Tympanix/automato/unit"
 
+// AllMetas returns a slice of all available units as meta objects
 func AllMetas() []*unit.Meta {
-	var metas []*unit.Meta
+	metas := make([]*unit.Meta, len(unit.Metas))
+	idx := 0
 	for _, v := range unit.Metas {
-		metas = append(metas, v)
+		metas[idx] = v
+		idx++
 	}
 	return metas
 }
