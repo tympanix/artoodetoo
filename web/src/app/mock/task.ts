04779ee24c2f8b864fc27f1636831634e804bceb
Mikkel Møller Brusen <Mikkel_mb@hotmail.com>
2017-03-17 12:14:26 +0100

    Cleanup from meta class

--- web/src/app/mock/task.ts
+++ web/src/app/mock/task.ts
@@ -1,44 +1,103 @@
 import { Task } from '../task';
 
-export const TASK: Task = {
-	"name": "MyFirstTask4",
-	"event": {
-		"id": "example.PersonEvent",
-		"name": "Person",
-		"recipe": []
-	},
-	"actions": [{
-		"id": "example.StringConverter",
-		"name": "String",
-		"recipe": [{
-			"type": 1,
-			"argument": "Format",
-			"source": "",
-			"value": "Person %s would like to say hello"
-		}, {
-			"type": 0,
-			"argument": "Placeholder",
-			"source": "Person",
-			"value": "Name"
-		}]
-	},{
-		"id": "example.EmailAction",
-		"name": "Email",
-		"recipe": [{
-			"type": 0,
-			"argument": "Message",
-			"source": "String",
-			"value": "Formatted"
-		}, {
-			"type": 1,
-			"argument": "Subject",
-			"source": "",
-			"value": "A new friend"
-		}, {
-			"type": 1,
-			"argument": "Receiver",
-			"source": "",
-			"value": "johndoe@email.com"
-		}]
-	}]
+export const TASK = {
+  "name": "MyFirstTask",
+  "event": {
+    "id": "example.PersonEvent",
+    "name": "Person",
+		description: "string",
+    "input": [],
+    "output": [
+      {
+        "name": "Name",
+        "type": "string"
+      },
+      {
+        "name": "Age",
+        "type": "int"
+      },
+      {
+        "name": "Height",
+        "type": "float32"
+      },
+      {
+        "name": "Married",
+        "type": "bool"
+      }
+    ]
+  },
+  "actions": [
+    {
+      "id": "example.StringConverter",
+      "name": "String",
+			description: "string",
+      "input": [
+        {
+          "name": "Format",
+          "type": "string",
+          "recipe": [
+            {
+              "type": 1,
+              "value": "Hello my name is %s"
+            }
+          ]
+        },
+        {
+          "name": "Placeholder",
+          "type": "interface {}",
+          "recipe": [
+            {
+              "type": 0,
+              "source": "Person",
+              "value": "Name"
+            }
+          ]
+        }
+      ],
+      "output": [
+        {
+          "name": "Formatted",
+          "type": "string"
+        }
+      ]
+    },
+    {
+      "id": "example.EmailAction",
+      "name": "Email",
+			description: "string",
+      "input": [
+        {
+          "name": "Message",
+          "type": "string",
+          "recipe": [
+            {
+              "type": 0,
+              "source": "String",
+              "value": "Formatted"
+            }
+          ]
+        },
+        {
+          "name": "Subject",
+          "type": "string",
+          "recipe": [
+            {
+              "type": 1,
+              "value": "A new friend"
+            }
+          ]
+        },
+        {
+          "name": "Receiver",
+          "type": "string",
+          "recipe": [
+            {
+              "type": 1,
+              "value": "johndoe@email.com"
+            }
+          ]
+        }
+      ]
+    }
+  ]
 }
