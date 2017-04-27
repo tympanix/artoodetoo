d8d37905013c717fda7d475370c57368937c7482
Mikkel Møller Brusen <Mikkel_mb@hotmail.com>
2017-04-27 20:18:49 +0200

    fix not including event

---
+++ web/src/app/event/event.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MdSnackBar } from '@angular/material';
+
+import { ApiService } from '../api.service';
+import { Event } from '../model';
+
+@Component({
+  selector: 'app-event',
+  templateUrl: './event.component.html',
+  styles: []
+})
+export class EventComponent implements OnInit {
+  @Input() event: Event
+
+  // Temporary placeholder when changing event name
+  eventname: string = ""
+
+  constructor(private api: ApiService, private snackBar: MdSnackBar) {}
+
+  ngOnInit() { }
+
+  changeEventName(name: string) {
+    if (!name) {
+      this.snackBar.open("The event must be given a name", "", { duration: 4000 })
+      return
+    }
+
+      this.event.name = name
+      this.eventname = ""
+
+  }
+
+}
