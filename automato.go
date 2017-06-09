d89041cf12fd3afa7a0fa308b62e0bced7ad49bd
Mathias <tympanix@gmail.com>
2017-06-09 14:49:57 +0200

    Fixed angular routes from golang web server

--- automato.go
+++ automato.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"path"
 	"strconv"
 
 	"github.com/Tympanix/artoodetoo/api"
@@ -10,11 +12,11 @@ import (
 	"github.com/Tympanix/artoodetoo/config"
 	"github.com/Tympanix/artoodetoo/storage"
 	"github.com/Tympanix/artoodetoo/types"
+	"github.com/gorilla/mux"
 )
 
 const (
-	apiroot  = "/api"
-	authroot = "/auth"
+	static = "web/dist/"
 )
 
 func main() {
@@ -28,20 +30,42 @@ func serve(args types.AppArgs) {
 	// Set up storage driver
 	initStorage()
 
+	router := mux.NewRouter()
+
 	// Set up api handler
-	http.Handle(apiroot+"/", http.StripPrefix(apiroot, api.API))
+	//router.Handle(apiroot, http.StripPrefix(apiroot, api.API))
+	router.PathPrefix("/api/").Handler(http.StripPrefix("/api", api.API))
 
 	// Set up file server for static files
-	fs := http.FileServer(http.Dir("web/dist"))
-	http.Handle("/", fs)
+	fs := newFileServer(static)
+	router.PathPrefix("/").Handler(fs)
 
 	// Serve the web server
 	log.Printf("Listening on port %d\n", config.Port)
-	log.Fatal(http.ListenAndServe(addr(), nil))
+	log.Fatal(http.ListenAndServe(addr(), router))
 }
 
 func addr() string {
 	return ":" + strconv.Itoa(config.Port)
+}
+
+type fileServer struct {
+	http.Handler
+}
+
+func newFileServer(path string) *fileServer {
+	return &fileServer{http.FileServer(http.Dir(static))}
+}
+
+func (f *fileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	file := path.Clean(r.URL.Path)
+	log.Println(file)
+	if fi, err := os.Stat(path.Join(static, file)); err == nil && !fi.IsDir() {
+		f.Handler.ServeHTTP(w, r)
+	} else {
+		log.Println("Serve index")
+		http.ServeFile(w, r, path.Join(static, "index.html"))
+	}
 }
 
 func initStorage() {
